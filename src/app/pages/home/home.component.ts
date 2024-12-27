a0ee24d5a98940acc30d5ef5e6ecaecfe7c1572f
Bruno Carneiro <bruno.ferreira@trf1.jus.br>
2024-12-27 18:44:46 -0300

    deixando a tabela de requisições de pagamento scrollavel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -35,6 +35,114 @@ export class HomeComponent {
         assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
         vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
       },
+      {
+        id: 3,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 4,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 5,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 6,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 7,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 8,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 9,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 10,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 11,
+        numero: '1018293-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 12,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 13,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 14,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 15,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 16,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 17,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 18,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 19,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
+      {
+        id: 20,
+        numero: '1018277-31.2022.4.01.3600',
+        assunto: 'AUXÍLIO-DOENÇA PREVIDENCIÁRIO',
+        vara: '9ª VARA FEDERAL DE JUIZADO ESPECIAL CÍVEL DA SJMT'
+      },
     ]
   }
 }
