301b74cbf51707187b6cada09c0d7aca16bdfcad
codethebasics <codethebasics@gmail.com>
2025-01-06 11:16:02 -0300

    preferencias

--- src/app/pages/configuracoes/preferencias/preferencias.component.ts
+++ src/app/pages/configuracoes/preferencias/preferencias.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { Card } from 'primeng/card';
 import { Image } from 'primeng/image';
+import { InputText } from 'primeng/inputtext';
+import { ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
+import { CheckIcon, TrashIcon } from 'primeng/icons';
 
 @Component({
   selector: 'app-preferencias',
-  imports: [Card, Image],
+  imports: [
+    Card,
+    Image,
+    InputText,
+    ButtonDirective,
+    CheckIcon,
+    ButtonIcon,
+    ButtonLabel,
+    TrashIcon,
+  ],
   templateUrl: './preferencias.component.html',
   styleUrl: './preferencias.component.scss',
   standalone: true,
