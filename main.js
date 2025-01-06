ad519354b34180e40106097e48c2add951e0be04
Bruno Carneiro <bruno.ferreira@trf1.jus.br>
2025-01-06 18:15:08 -0300

    permissao

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 800});
-  win.loadFile('dist/sirea-front/browser/index.html');
+  win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    icon: path.join(__dirname, 'public', 'sirea-ico-256x256.png'),
+  });
+  win.loadFile('dist/sirea-front/browser/index.csr.html');
+
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
-})
+});
--- src/app/pages/configuracoes/preferencias/preferencias.component.ts
+++ src/app/pages/configuracoes/preferencias/preferencias.component.ts
@@ -3,7 +3,9 @@ import { Card } from 'primeng/card';
 import { Image } from 'primeng/image';
 import { InputText } from 'primeng/inputtext';
 import { ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
-import { CheckIcon, TrashIcon } from 'primeng/icons';
+import { CheckIcon, TrashIcon, UndoIcon } from 'primeng/icons';
+import { FormsModule } from '@angular/forms';
+import { Tree } from 'primeng/tree';
 
 @Component({
   selector: 'app-preferencias',
@@ -16,9 +18,91 @@ import { CheckIcon, TrashIcon } from 'primeng/icons';
     ButtonIcon,
     ButtonLabel,
     TrashIcon,
+    FormsModule,
+    Tree,
+    UndoIcon,
+
   ],
   templateUrl: './preferencias.component.html',
   styleUrl: './preferencias.component.scss',
   standalone: true,
 })
-export class PreferenciasComponent {}
+export class PreferenciasComponent {
+  groupedProfiles: any[];
+  selectedProfile: any[];
+
+  constructor() {
+    this.groupedProfiles = [
+      {
+        label: 'Administrador',
+        value: 'admtrf1',
+        selected: true,
+        children: [
+          {
+            label: 'Gestão',
+            value: 'ges',
+            selected: false,
+            children: [
+              { label: 'Gestão de precatório diretor', value: 'gpd', selected: false, },
+              { label: 'Gestão de precatório supervisor', value: 'gsu', selected: false, },
+              { label: 'Gestão de precatório servidor', value: 'gps', selected: false, },
+            ],
+          },
+          {
+            label: 'Vara',
+            value: 'var',
+            selected: false,
+            children: [
+              { label: 'Diretor de vara', value: 'vardir', selected: false, },
+              { label: 'Servidor de vara', value: 'varser', selected: false, },
+              { label: 'Colaborador de vara', value: 'varcolab', selected: false, },
+              { label: 'Estagiário de vara', value: 'varestag', selected: false, },
+            ],
+          },
+          {
+            label: 'DIEFI',
+            value: 'dif',
+            selected: false,
+            children: [
+              { label: 'Diretor da DIEFI', value: 'gdie', selected: false, },
+              { label: 'Supervisor da DIEFI', value: 'gdsu', selected: false, },
+              { label: 'Servidor da DIEFI', value: 'gdies', selected: false },
+            ],
+          },
+          {
+            label: 'Magistrado',
+            value: 'mag',
+            selected: false,
+          },
+          {
+            label: 'Advogado',
+            value: 'adv',
+            selected: false,
+          },
+          {
+            label: 'Funcionários de escritórios de advocacia',
+            value: 'advfunc',
+            selected: false,
+          },
+          {
+            label: 'Executado',
+            value: 'executado',
+            selected: false,
+          },
+          {
+            label: 'Sistema cliente',
+            value: 'sis',
+            selected: false,
+          },
+          {
+            label: 'Servidor do executado',
+            value: 'prepexec',
+            selected: false,
+          },
+        ],
+      },
+    ];
+
+    this.selectedProfile = [];
+  }
+}
